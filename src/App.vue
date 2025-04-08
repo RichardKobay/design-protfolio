f2fb854ecea05ac4849e049dfe6d2e132a391bf3
Juan Ricardo Soriano Roel <ricardo.roel110@gmail.com>
2025-04-07 21:17:17 -0600

    changes

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
       class="container px-6 mx-auto md:flex md:justify-between md:items-center"
     >
       <div class="flex items-center justify-between">
-        <div class="relative w-16 h-16">
+        <div class="relative w-16 h-16 overflow-hidden">
           <img
-            class="border border-gray-100 rounded-full shadow-sm"
-            src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=512&h=512&q=80"
+            class="overflow-hidden border border-gray-100 rounded-full shadow-sm over"
+            src="./assets/yo-cortado.jpg"
             alt="user image"
           />
         </div>
@@ -58,19 +58,23 @@
             target="_blank"
             class="text-black hover:underline"
           >
-            Ricardo Soriano</a
-          >🙇‍♀️<br />Creative Designer and Storyteller
+            RichardKobay</a
+          >🙇‍♀️<br />Creative Designer and Storyteller (also a great developer)
         </h1>
 
         <div class="gap-1 md:flex md:gap-10">
-          <button class="hidden text-white bg-gray-800 submit-button md:block">
-            View My Work
-          </button>
-          <button
-            class="mb-5 text-gray-800 border-4 border-gray-800 submit-button md:mb-0"
-          >
-            Contact Me
-          </button>
+          <a href="#work" v-smooth-scroll>
+            <button class="hidden text-white bg-gray-800 submit-button md:block">
+              View My Work
+            </button>
+          </a>
+          <a href="#about" v-smooth-scroll>
+            <button
+              class="mb-5 text-gray-800 border-4 border-gray-800 submit-button md:mb-0"
+            >
+              Contact Me
+            </button>
+          </a>
         </div>
       </div>
 
@@ -94,9 +98,11 @@
           app is designed in a way that it helps the user to take care of their
           plants.
         </p>
-        <button class="text-2xl font-bold tracking-wider text-sky-800">
-          View Case Study
-        </button>
+        <a href="https://www.figma.com/deck/rVa24iWDEpeKftZF4yP4qU/Untitled?node-id=1-42&t=ipwi45wvmUtdP7wd-1">
+          <button class="text-2xl font-bold tracking-wider text-sky-800">
+            View Case Study
+          </button>
+        </a>
       </div>
       <div class="bg-green-100 card-image">
         <img
@@ -108,16 +114,17 @@
 
     <div class="card">
       <div class="px-8 py-8 space-y-5 md:py-16 md:px-20 md:w-1/2">
-        <h4 class="project-title item">Sheepify States</h4>
+        <h4 class="project-title item">Nutricionsita</h4>
         <p class="pr-12 font-work_sans">
-          Designed some empty state screens in kawaii style for the popular
-          sheepify app.Tailwind lets you conditionally apply utility classes in
-          different states using variant modifiers. For example, use hover:gap-6
-          to only apply the gap-6 utility
+          Designed an app that helps people to take care of their nutrition. The
+          app is designed in a way that it helps the user to take care of their
+          health and nutrition. The app is useful and easy to use, not to mention that is pretty beautiful.
         </p>
-        <button class="text-2xl font-bold tracking-wider text-sky-800">
-          View Case Study
-        </button>
+        <a href="https://www.figma.com/deck/WRFv2MXUkmrgjfHhN6B9e3/Nutricionsita---Slides?node-id=1-42&viewport=-34%2C-96%2C0.47&t=IIltKJjogp41ptM5-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1">
+          <button class="text-2xl font-bold tracking-wider text-sky-800">
+            View Case Study
+          </button>
+        </a>
       </div>
       <div class="card-image">
         <img
@@ -143,22 +150,10 @@
           >
             <img
               class="absolute object-cover w-full h-full"
-              src="https://a.slack-edge.com/80588/marketing/img/meta/slack_hash_256.png"
+              src="./assets/cubosls-logo.png"
               alt=""
             />
             <div class="absolute inset-0 bg-gray-700 opacity-75"></div>
-            <svg
-              class="relative"
-              width="200"
-              height="120"
-              viewBox="0 0 200 120"
-            >
-              <path
-                fill="#fff"
-                fill-rule="evenodd"
-                d="M63.75 75.24l2.5-5.93c2.7 2.05 6.3 3.13 9.85 3.13 2.61 0 4.27-1.03 4.27-2.6-.04-4.37-15.7-.95-15.82-11.9-.04-5.58 4.8-9.86 11.66-9.86 4.08 0 8.16 1.03 11.06 3.38l-2.34 6.05c-2.66-1.73-5.97-2.97-9.12-2.97-2.13 0-3.55 1.03-3.55 2.36.04 4.28 15.82 1.94 15.98 12.4 0 5.7-4.72 9.69-11.5 9.69-4.96 0-9.52-1.2-12.99-3.75zm96.01-8.08a7.16 7.16 0 0 1-8.11 3.5 7.35 7.35 0 0 1-5.38-7.13c0-3.34 2.2-6.27 5.38-7.12 3.16-.85 6.5.58 8.11 3.5l6.9-3.93a15.02 15.02 0 0 0-17.03-7.42 15.42 15.42 0 0 0-11.33 14.97c0 7.04 4.66 13.2 11.33 14.97a15.02 15.02 0 0 0 17.04-7.43l-6.9-3.9zM91.71 35.25h8.64v43.16h-8.64V35.25zm78.34 0v43.16h8.65V65.48l10.25 12.93H200l-13.03-15.37 12.06-14.34h-10.58l-9.76 11.9V35.25h-8.64zm-43.82 31.1a8.14 8.14 0 0 1-6.83 3.56 7.24 7.24 0 0 1-7.33-7.16 7.24 7.24 0 0 1 7.33-7.15 8.1 8.1 0 0 1 6.83 3.67v7.08zm0-18v3.4c-1.43-2.36-5-4-8.72-4-7.7 0-13.76 6.64-13.76 14.96 0 8.33 6.07 15.04 13.76 15.04 3.73 0 7.3-1.63 8.72-4v3.4H135v-28.8h-8.77zM10.53 66.4A5.32 5.32 0 0 1 5.3 71.8 5.32 5.32 0 0 1 .04 66.4a5.32 5.32 0 0 1 5.25-5.38h5.25v5.38zm2.63 0a5.32 5.32 0 0 1 5.25-5.38c2.9 0 5.25 2.4 5.25 5.38v13.46a5.32 5.32 0 0 1-5.25 5.39 5.32 5.32 0 0 1-5.25-5.39V66.4zm5.26-21.63a5.32 5.32 0 0 1-5.25-5.38A5.32 5.32 0 0 1 18.42 34c2.9 0 5.25 2.41 5.25 5.39v5.38h-5.25zm0 2.74c2.9 0 5.25 2.41 5.25 5.39a5.32 5.32 0 0 1-5.25 5.38H5.25A5.32 5.32 0 0 1 0 52.9a5.32 5.32 0 0 1 5.25-5.39h13.17zm21.03 5.39a5.32 5.32 0 0 1 5.25-5.39c2.9 0 5.25 2.41 5.25 5.39a5.32 5.32 0 0 1-5.25 5.38h-5.25V52.9zm-2.62 0a5.32 5.32 0 0 1-5.25 5.38 5.32 5.32 0 0 1-5.25-5.38V39.39A5.32 5.32 0 0 1 31.58 34c2.9 0 5.25 2.41 5.25 5.39v13.5zm-5.25 21.58c2.9 0 5.25 2.41 5.25 5.38a5.32 5.32 0 0 1-5.25 5.39 5.32 5.32 0 0 1-5.25-5.39v-5.38h5.25zm0-2.7a5.32 5.32 0 0 1-5.25-5.38 5.32 5.32 0 0 1 5.25-5.38h13.17c2.9 0 5.25 2.4 5.25 5.38a5.32 5.32 0 0 1-5.25 5.39H31.58z"
-              />
-            </svg>
           </div>
           <div class="relative bg-white lg:w-7/12">
             <svg
@@ -172,16 +167,16 @@
             <div
               class="relative px-8 py-12 leading-relaxed text-gray-700 lg:py-24 lg:px-16"
             >
+              <h2 class="text-5xl font-bold">CUBOS LS</h2>
               <p>
                 As
-                <strong class="font-medium text-gray-900">Slack</strong> grows
-                rapidly, using Stripe helps them scale payments easily &mdash;
-                supporting everything from getting paid by users around the
-                world to enabling ACH payments for corporate customers.
+                <strong class="font-medium text-gray-900">Cubos LS</strong> grows
+                rapidly, working with RichardKobay is pretty much like working with
+                a friend. He is always there to help and support us.
               </p>
               <p class="mt-6">
                 <a
-                  href="#"
+                  href="http://cubosls.com/"
                   class="font-medium text-indigo-600 hover:text-indigo-900"
                 >
                   &rarr; Md Solaiman Hossain</a
@@ -231,15 +226,11 @@
       alt="user image"
     />
     <div class="px-8 py-8 space-y-5 md:py-16 md:px-20 md:w-1/2">
-      <h4 class="project-title item">Sheepify States</h4>
+      <h4 class="project-title item">About Me</h4>
       <p class="font-work_sans">
-        I am Salman Khan from Lucknow, India. I am working in an Indian Startup
-        as a User Eperience Designer for the past two years.
-        <br />
-        I got featured various times on many big and small marketplaces,
-        portfolio websites and blogs. I also received some awards and
-        recognitions from some of the big and small award companies. Besides
-        designing, I like to watch anime, read books and solve sudoku.
+        I am RichardKobay (Ricardo Soriano), a designer and storyteller. I am a
+        designer who loves to create beautiful and functional designs. I like anime
+        and develop beautiful things.
       </p>
       <button class="text-2xl font-bold tracking-wider text-sky-800">
         View Case Study
@@ -252,41 +243,45 @@
       <h2 class="mb-5 text-2xl font-bold text-gray-700 md:text-6xl font-Eczar">
         Let’s work together and make everything super cute and super useful.
       </h2>
-      <a class="text-2xl text-blue-600 underline font-work_sans"
-        >📧 hire@author.com</a
+      <a class="text-2xl text-blue-600 underline font-work_sans" href="mailto:ricardo.roel110@gmail.com"
+        >📧 ricardo.roel110@gmail.com</a
       >
     </div>
     <div class="w-5/12 pr-28">
       <div class="flex flex-wrap justify-end gap-2">
-        <button
-          class="inline-flex items-center p-2 space-x-2 font-semibold text-white bg-gray-700 rounded"
-        >
-          <svg
-            class="w-4 h-4 fill-current md:w-10 md:h-10 lg:w-10 lg:h-10 xl:h-10 xl:w-10"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
+        <a href="https://www.facebook.com/ricardo.roel.98">
+          <button
+            class="inline-flex items-center p-2 space-x-2 font-semibold text-white bg-gray-700 rounded"
           >
-            <path
-              d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
-            />
-          </svg>
-        </button>
+            <svg
+              class="w-4 h-4 fill-current md:w-10 md:h-10 lg:w-10 lg:h-10 xl:h-10 xl:w-10"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+              />
+            </svg>
+          </button>
+        </a>
 
-        <button
-          class="inline-flex items-center p-2 space-x-2 font-semibold text-white bg-gray-700 rounded"
-        >
-          <svg
-            class="w-4 h-4 fill-current md:w-10 md:h-10 lg:w-10 lg:h-10 xl:h-10 xl:w-10"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
+        <a href="https://x.com/RichardKobay">
+          <button
+            class="inline-flex items-center p-2 space-x-2 font-semibold text-white bg-gray-700 rounded"
           >
-            <path
-              d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
-            />
-          </svg>
-        </button>
+            <svg
+              class="w-4 h-4 fill-current md:w-10 md:h-10 lg:w-10 lg:h-10 xl:h-10 xl:w-10"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
+              />
+            </svg>
+          </button>
+        </a>
 
         <button
           class="inline-flex items-center p-2 space-x-2 font-semibold text-white bg-gray-700 rounded"
@@ -318,22 +313,24 @@
           </svg>
         </button>
 
-        <button
-          class="inline-flex items-center p-2 space-x-2 font-semibold text-white bg-gray-700 rounded"
-        >
-          <svg
-            class="w-4 h-4 fill-current md:w-10 md:h-10 lg:w-10 lg:h-10 xl:h-10 xl:w-10"
-            role="img"
-            viewBox="0 0 256 256"
-            xmlns="http://www.w3.org/2000/svg"
+        <a href="https://www.linkedin.com/in/juan-ricardo-soriano-roel-78514321b/">
+          <button
+            class="inline-flex items-center p-2 space-x-2 font-semibold text-white bg-gray-700 rounded"
           >
-            <g>
-              <path
-                d="M218.123122,218.127392 L180.191928,218.127392 L180.191928,158.724263 C180.191928,144.559023 179.939053,126.323993 160.463756,126.323993 C140.707926,126.323993 137.685284,141.757585 137.685284,157.692986 L137.685284,218.123441 L99.7540894,218.123441 L99.7540894,95.9665207 L136.168036,95.9665207 L136.168036,112.660562 L136.677736,112.660562 C144.102746,99.9650027 157.908637,92.3824528 172.605689,92.9280076 C211.050535,92.9280076 218.138927,118.216023 218.138927,151.114151 L218.123122,218.127392 Z M56.9550587,79.2685282 C44.7981969,79.2707099 34.9413443,69.4171797 34.9391618,57.260052 C34.93698,45.1029244 44.7902948,35.2458562 56.9471566,35.2436736 C69.1040185,35.2414916 78.9608713,45.0950217 78.963054,57.2521493 C78.9641017,63.090208 76.6459976,68.6895714 72.5186979,72.8184433 C68.3913982,76.9473153 62.7929898,79.26748 56.9550587,79.2685282 M75.9206558,218.127392 L37.94995,218.127392 L37.94995,95.9665207 L75.9206558,95.9665207 L75.9206558,218.127392 Z M237.033403,0.0182577091 L18.8895249,0.0182577091 C8.57959469,-0.0980923971 0.124827038,8.16056231 -0.001,18.4706066 L-0.001,237.524091 C0.120519052,247.839103 8.57460631,256.105934 18.8895249,255.9977 L237.033403,255.9977 C247.368728,256.125818 255.855922,247.859464 255.999,237.524091 L255.999,18.4548016 C255.851624,8.12438979 247.363742,-0.133792868 237.033403,0.000790807055"
-              ></path>
-            </g>
-          </svg>
-        </button>
+            <svg
+              class="w-4 h-4 fill-current md:w-10 md:h-10 lg:w-10 lg:h-10 xl:h-10 xl:w-10"
+              role="img"
+              viewBox="0 0 256 256"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <path
+                  d="M218.123122,218.127392 L180.191928,218.127392 L180.191928,158.724263 C180.191928,144.559023 179.939053,126.323993 160.463756,126.323993 C140.707926,126.323993 137.685284,141.757585 137.685284,157.692986 L137.685284,218.123441 L99.7540894,218.123441 L99.7540894,95.9665207 L136.168036,95.9665207 L136.168036,112.660562 L136.677736,112.660562 C144.102746,99.9650027 157.908637,92.3824528 172.605689,92.9280076 C211.050535,92.9280076 218.138927,118.216023 218.138927,151.114151 L218.123122,218.127392 Z M56.9550587,79.2685282 C44.7981969,79.2707099 34.9413443,69.4171797 34.9391618,57.260052 C34.93698,45.1029244 44.7902948,35.2458562 56.9471566,35.2436736 C69.1040185,35.2414916 78.9608713,45.0950217 78.963054,57.2521493 C78.9641017,63.090208 76.6459976,68.6895714 72.5186979,72.8184433 C68.3913982,76.9473153 62.7929898,79.26748 56.9550587,79.2685282 M75.9206558,218.127392 L37.94995,218.127392 L37.94995,95.9665207 L75.9206558,95.9665207 L75.9206558,218.127392 Z M237.033403,0.0182577091 L18.8895249,0.0182577091 C8.57959469,-0.0980923971 0.124827038,8.16056231 -0.001,18.4706066 L-0.001,237.524091 C0.120519052,247.839103 8.57460631,256.105934 18.8895249,255.9977 L237.033403,255.9977 C247.368728,256.125818 255.855922,247.859464 255.999,237.524091 L255.999,18.4548016 C255.851624,8.12438979 247.363742,-0.133792868 237.033403,0.000790807055"
+                ></path>
+              </g>
+            </svg>
+          </button>
+        </a>
       </div>
     </div>
   </div>
